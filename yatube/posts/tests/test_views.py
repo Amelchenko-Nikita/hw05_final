b9b90a193aab31adef4c6e253cbe2e9746dfd13f
Amelchenko-Nikita <104903272+Amelchenko-Nikita@users.noreply.github.com>
2022-07-17 16:31:14 +0300

    Update test_views.py

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,5 +1,4 @@
 from django import forms
-
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.cache import cache
@@ -260,9 +259,8 @@ class FollowTests(TestCase):
                                               self.user_following.username}))
         self.assertEqual(Follow.objects.all().count(), 0)
 
-     def test_subscription_feed(self):
+    def test_subscription_feed(self):
         """запись появляется в ленте подписчиков"""
- 
         Follow.objects.create(user=self.user_follower,
                               author=self.user_following)
         response = self.client_auth_follower.get(reverse('posts:follow_index'))
