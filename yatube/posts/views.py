c768b66e6dfb82d8ff10940238b5a3ec35305009
Amelchenko-Nikita <104903272+Amelchenko-Nikita@users.noreply.github.com>
2022-07-17 12:49:35 +0300

    Update views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
-from .forms import CommentForm, CreateForm, PostForm
+from .forms import CommentForm, PostForm
 from .models import Follow, Group, Post, User
 from .utils import page_paginator
 
